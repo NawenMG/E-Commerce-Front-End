56ef02d98acdf5e6fa5fe015e6110bbcdabb7a25
Mario <GranatoM@outlook.it>
2024-09-11 15:50:43 +0200

    HeaderHOME

---
+++ src/app/Home/header/header.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+   inputSearch = "";
+
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule, isPlatformBrowser, isPlatformServer } from '@angular/common';
 import { PComponent } from './p/p.component';
+import { HeaderComponent } from "./Home/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, PComponent],
+  imports: [RouterOutlet, CommonModule, PComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 
