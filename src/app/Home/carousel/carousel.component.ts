b8f9f2fdd8429c5b78f6a74af0655be8cd5ac848
Mario <GranatoM@outlook.it>
2024-09-12 17:32:03 +0200

    carousel

---
+++ src/app/Home/carousel/carousel.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel',
+  standalone: true,
+  imports: [],
+  templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.css'
+})
+export class CarouselComponent {
+
+  immagine1:string = "my-app/src/assets/immagine1.avif";
+  immagine2:string = "my-app/src/assets/immagine1.avif";
+  immagine3:string = "my-app/src/assets/immagine1.avif";
+
+  immagineCarousel:string = "immagineCarousel";
+
+
+
+
+
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { CommonModule, isPlatformBrowser, isPlatformServer } from '@angular/comm
 import { PComponent } from './p/p.component';
 import { HeaderComponent } from "./Home/header/header.component";
 import { DropdownsComponent } from './Home/dropdowns/dropdowns.component';
+import { CarouselComponent } from "./Home/carousel/carousel.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, PComponent, HeaderComponent, DropdownsComponent],
+  imports: [RouterOutlet, CommonModule, PComponent, HeaderComponent, DropdownsComponent, CarouselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 
