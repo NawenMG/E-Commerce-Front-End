124eb46aae54ceeebfb7cf8938ee1dbe07233afd
Mario <GranatoM@outlook.it>
2024-09-12 18:00:11 +0200

    g

---
+++ src/app/Home/carouselseries/carouselseries.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carouselseries',
+  standalone: true,
+  imports: [],
+  templateUrl: './carouselseries.component.html',
+  styleUrl: './carouselseries.component.css'
+})
+export class CarouselseriesComponent {
+
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,13 @@ import { PComponent } from './p/p.component';
 import { HeaderComponent } from "./Home/header/header.component";
 import { DropdownsComponent } from './Home/dropdowns/dropdowns.component';
 import { CarouselComponent } from "./Home/carousel/carousel.component";
+import { CarouselseriesComponent } from "./Home/carouselseries/carouselseries.component";
+import { FooterComponent } from "./Home/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, PComponent, HeaderComponent, DropdownsComponent, CarouselComponent],
+  imports: [RouterOutlet, CommonModule, PComponent, HeaderComponent, DropdownsComponent, CarouselComponent, CarouselseriesComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 
