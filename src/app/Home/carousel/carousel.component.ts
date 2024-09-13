3f5982fee4d3f8b824d09ef3d914fc182c6eae56
Mario <GranatoM@outlook.it>
2024-09-13 08:55:23 +0200

    ok

--- src/app/Home/carousel/carousel.component.ts
+++ src/app/Home/carousel/carousel.component.ts
@@ -9,11 +9,11 @@ import { Component } from '@angular/core';
 })
 export class CarouselComponent {
 
-  immagine1:string = "my-app/src/assets/immagine1.avif";
-  immagine2:string = "my-app/src/assets/immagine1.avif";
-  immagine3:string = "my-app/src/assets/immagine1.avif";
+  immagine1:string = "https://plus.unsplash.com/premium_photo-1683746792239-6ce8cdd3ac78?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8ZSUyMGNvbW1lcmNlfGVufDB8fDB8fHww";
+  immagine2:string = "https://plus.unsplash.com/premium_photo-1664201890375-f8fa405cdb7d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZSUyMGNvbW1lcmNlfGVufDB8fDB8fHww";
+  immagine3:string = "https://plus.unsplash.com/premium_photo-1683746792467-c6ae33d06c20?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGUlMjBjb21tZXJjZXxlbnwwfHwwfHx8MA%3D%3D";
 
-  immagineCarousel:string = "immagineCarousel";
+  immagineCarousel:string = "immagineCarousel"; //Nell attributo classe
 
 
 
