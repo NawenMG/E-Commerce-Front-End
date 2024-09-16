3c1e9bcd69637713ff3a1d8251d19cdbf1b1ca2a
Mario <GranatoM@outlook.it>
2024-09-16 10:46:05 +0200

    t

---
+++ src/app/Deliverydash/delivery-dash/delivery-dash.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delivery-dash',
+  standalone: true,
+  imports: [],
+  templateUrl: './delivery-dash.component.html',
+  styleUrl: './delivery-dash.component.css'
+})
+export class DeliveryDashComponent {
+
+}
