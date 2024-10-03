7e48f4dffc640c8a25f5e94547d66207254bc37e
nawen <GranatoM@outlook.it>
2024-10-03 07:59:33 +0200

    m

---
+++ src/app/appModule.module.ts
@@ -0,0 +1,28 @@
+/*import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';//Per l'idratazione
+import { AppComponent } from './app.component';
+import { PComponent } from './p/p.component';
+
+
+
+@NgModule({
+  declarations: [ //Per dichiarare una lista di componenti
+    AppComponent,
+    PComponent
+  ],
+  imports: [ //Per le librerie
+    BrowserModule, //Per usare le direttive
+  ],
+  providers: [ //Per l'injection
+    provideClientHydration() //Funzione per l'idratazione del ssr
+  ],
+  //exports:[ //Per esportare componenti secondari
+  //],
+  bootstrap:[ //Dichiarazione del componente principale per l'app-root
+    AppComponent
+  ],
+
+})
+export class AppModule { }
+*/
